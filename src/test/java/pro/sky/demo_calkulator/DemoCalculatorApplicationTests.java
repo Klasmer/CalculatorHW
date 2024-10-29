aa760b64adb65202796db8c246e63f3e428d31a9
klasmer <klasmer495@gmail.com>
2024-10-29 17:58:22 +0300

    Введение в Maven и Spring Boot

---
+++ src/test/java/pro/sky/demo_calkulator/DemoCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.demo_calkulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
